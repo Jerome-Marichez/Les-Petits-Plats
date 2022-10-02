165b2463890c155221fe8c04e4a075beeeca7d4d
Jerome Marichez <admin@digitstudio.fr>
2022-10-02 13:52:37 +0200

    Initail Comit

---
+++ src/scripts/main.js
@@ -0,0 +1,17 @@
+
+import { recipes } from "./data/recipes.js"; // IMPORT DATA 
+import { displayDefaultDOM } from "./dom/display.js";
+
+// defaultRecipes contains the recipes without filter or research
+// whiles recipes contains the recipes with or without filter / research
+var defaultRecipes = recipes; 
+
+
+// Function to init the code
+function init() { 
+
+}
+
+init(); 
+
+
