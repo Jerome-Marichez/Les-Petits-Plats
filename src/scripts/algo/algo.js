fa6e0c6a188adcee2354227569318012d256f416
Jerome Marichez <admin@digitstudio.fr>
2022-10-19 18:19:02 +0200

    fix Atchecker + W3C
    We fixed AtChecker issue + W3C

--- src/scripts/algo/algo.js
+++ src/scripts/algo/algo.js
@@ -1,14 +1,34 @@
-// Algo Modern
 export function searchAlgo(recipes, input) {
-    return recipes.filter(
-        (recipe) =>
-        (
-            recipe.name.toLowerCase().includes(input)
-            ||
-            recipe.ingredients.some((ingredient) => ingredient.ingredient.toLowerCase() === input)
-            ||
-            recipe.description.toLowerCase().includes(input)
-        )
-    );
+
+    const recipesFiltered = [];
+
+
+    for (let i = 0; i < recipes.length; i++) {
+        const recipe = recipes[i];
+        const recipeName = recipe.name.toLowerCase();
+        const recipeDescription = recipe.description.toLowerCase();
+
+        // Regex to check if description or name = input.
+        const regex = new RegExp('(?:^|\\s)' + input.toLowerCase(), 'i');
+        const isNameContains = regex.test(recipeName);
+        const isDescriptionContains = regex.test(recipeDescription);
+
+        // If input contains  Ingredients, Name, Description contains then PUSH
+        for (let i = 0; i < recipe.ingredients.length; i++) {
+            const ingredients = recipe.ingredients[i];
+
+            const isIngredientContains = regex.test(ingredients.ingredient);
+
+            if (isIngredientContains || isNameContains || isDescriptionContains) {
+                recipesFiltered.push(recipe);
+            }
+
+        }
+    }
+
+
+    return [...new Set(recipesFiltered)];
+
+
 };
-// Fin Algo Modern
+
