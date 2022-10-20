47b46641af870c0e67c3555647afe263995c92bb
Jerome Marichez <jeromemarichez@ik.me>
2022-10-20 22:30:14 +0200

    Update filter items <li>
    We fixed the issue by create a function clearFilter() who clear filter <li> items and using displayItems() when neededed in updateResearch() function inside searchbar.JS to display Filter Items depend of search request

--- src/scripts/dom/clear.js
+++ src/scripts/dom/clear.js
@@ -4,7 +4,28 @@ export function clearCards(cardsContainer) {
     cardsContainer.innerHTML = "";
 }
 
+export function clearFilter(filterList) {
+    let filterAppliances = false, filterUstensils = false, filterIngredients = false;
+
+    filterList.forEach(element => {
+        console.log(element);
+        if (element.className.includes("appliances")) {
+            filterAppliances = document.getElementsByClassName(element.className)[0];
+        }
+        if (element.className.includes("ustensils")) {
+            filterUstensils = document.getElementsByClassName(element.className)[0];
+        }
+        if (element.className.includes("ingredients")) {
+            filterIngredients = document.getElementsByClassName(element.className)[0];
+        }
+    });
+
+    filterAppliances.innerHTML = "";
+    filterUstensils.innerHTML = "";
+    filterIngredients.innerHTML = "";
+}
+
 export function clearSelectedFilter(selectedFilter) {
     selectedFilter.remove();
-    updateResearch(); 
+    updateResearch();
 }
